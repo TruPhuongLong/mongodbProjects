d409e2b13216f9401f7bef67bdddb2c170aa8b7e
TruPhuongLong <longbaloca@gmail.com>
2018-06-05 13:26:21 +0700

    get todos

--- mongo-todos-api/src/server/server.test.js
+++ mongo-todos-api/src/server/server.test.js
@@ -10,45 +10,45 @@ beforeEach(done => {
 })
 
 describe('Post /todos', () => {
-    // it('should create a new Todo', (done) => {
-    //     const text = 'something to do'
+    it('should create a new Todo', (done) => {
+        const text = 'something to do'
 
-    //     request(app)
-    //         .post('/todos')
-    //         .send({ text })
-    //         .expect(200)
-    //         .expect(res => {
-    //             expect(res.body.text).toBe(text)
-    //         })
-    //         .end((error, res) => {
-    //             if (error) {
-    //                 return done(error);
-    //             }
-    //             Todo.find()
-    //                 .then(docs => {
-    //                     expect(docs.length).toBe(1)
-    //                     expect(docs[0].text).toBe(text)
-    //                     done()
-    //                 })
-    //                 .catch(error => done(error))
-    //         });
-    // })
-
-    it('should be invalid post todo', done => {
         request(app)
-        .post('/todos')
-        .send({})
-        .expect(400)
-        .end((error, res)=>{
-            if(error){
-                done(error)
-            }
-            Todo.find()
-            .then(todos => {
-                expect(todos.length).toBe(0)
-                done()
+            .post('/todos')
+            .send({ text })
+            .expect(200)
+            .expect(res => {
+                expect(res.body.text).toBe(text)
             })
-            .catch(error => done(error))
-        })
+            .end((error, res) => {
+                if (error) {
+                    return done(error);
+                }
+                Todo.find()
+                    .then(docs => {
+                        expect(docs.length).toBe(1)
+                        expect(docs[0].text).toBe(text)
+                        done()
+                    })
+                    .catch(error => done(error))
+            });
     })
+
+    // it('should be invalid post todo', done => {
+    //     request(app)
+    //     .post('/todos')
+    //     .send({})
+    //     .expect(400)
+    //     .end((error, res)=>{
+    //         if(error){
+    //             done(error)
+    //         }
+    //         Todo.find()
+    //         .then(todos => {
+    //             expect(todos.length).toBe(0)
+    //             done()
+    //         })
+    //         .catch(error => done(error))
+    //     })
+    // })
 })
