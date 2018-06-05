eff21e58e569f8f176b6b563682c297e9a84c86c
TruPhuongLong <longbaloca@gmail.com>
2018-06-05 15:13:40 +0700

    get with id, test case

--- mongo-todos-api/src/server/server.js
+++ mongo-todos-api/src/server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyparser = require('body-parser');
 
 const { mongoose } = require('./db/mongoose');
+const {ObjectID} = require('mongodb');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 
@@ -12,18 +13,37 @@ const port = process.env.PORT || 3000;
 app.use(bodyparser.json());
 
 //request:
+// request all todos:
 app.get('/api/todos', (req, res) => {
-    Todo.find().then(docs => {
-        res.send(docs)
+    Todo.find()
+    .then(todos => {
+        res.send({ todos })
     })
     .catch(error => {
         res.status(400).send(error)
     })
 })
 
-app.post('/todos', (req, res) => {
+// request todo with id:
+app.get('/api/todos/:id', (req, res) => {
+    const id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        res.status(404).send()
+    }
+    Todo.findById(id)
+    .then(todo => {
+        if(!todo){
+            res.status(404).send()
+        }
+        res.send({todo})
+    })
+    .catch(error => res.status(400).send(error))
+})
+
+// post todo:
+app.post('/api/todos', (req, res) => {
     console.log('post todo')
-    const newTodo = new Todo({ text: req.body.text})
+    const newTodo = new Todo({ text: req.body.text })
     newTodo.save()
         .then(doc => res.send(doc))
         .catch(error => res.status(400).send(error))
@@ -31,7 +51,7 @@ app.post('/todos', (req, res) => {
 
 app.listen(port, () => console.log(`server listen on port ${port}`));
 
-module.exports = {app}
+module.exports = { app }
 
 
 
--- mongo-todos-api/src/server/server.test.js
+++ mongo-todos-api/src/server/server.test.js
@@ -1,54 +1,102 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectId} = require('mongodb');
 
 const { app } = require('./server');
 const { Todo } = require('./models/todo');
 
+//================================Test post=========================================
+// beforeEach(done => {
+//     Todo.remove()
+//         .then(() => done())
+// })
+
+// describe('Post /todos', () => {
+//     it('should create a new Todo', (done) => {
+//         const text = 'something to do'
+
+//         request(app)
+//             .post('/api/todos')
+//             .send({ text })
+//             .expect(200)
+//             .expect(res => {
+//                 expect(res.body.text).toBe(text)
+//             })
+//             .end((error, res) => {
+//                 if (error) {
+//                     return done(error);
+//                 }
+//                 Todo.find()
+//                     .then(docs => {
+//                         expect(docs.length).toBe(1)
+//                         expect(docs[0].text).toBe(text)
+//                         done()
+//                     })
+//                     .catch(error => done(error))
+//             });
+//     })
+
+//     // it('should be invalid post todo', done => {
+//     //     request(app)
+//     //     .post('/api/todos')
+//     //     .send({})
+//     //     .expect(400)
+//     //     .end((error, res)=>{
+//     //         if(error){
+//     //             done(error)
+//     //         }
+//     //         Todo.find()
+//     //         .then(todos => {
+//     //             expect(todos.length).toBe(0)
+//     //             done()
+//     //         })
+//     //         .catch(error => done(error))
+//     //     })
+//     // })
+// })
+
+
+
+
+// =================================test get /api/todos/:id ===========================
+const todos = [
+    { text: "first todo with id", _id: new ObjectId() },
+    { text: "second todo with id", _id: new ObjectId() },
+]
+
 beforeEach(done => {
-    Todo.remove()
-        .then(() => done())
+    Todo.remove().then(()=>{
+        Todo.insertMany(todos)
+    })
+    .then(done)
+
 })
 
-describe('Post /todos', () => {
-    it('should create a new Todo', (done) => {
-        const text = 'something to do'
+describe('test GET /api/todos/id', () => {
+    // it('should get todo with id', (done) => {
+    //     request(app)
+    //     .get(`/api/todos/${todos[0]._id}`)
+    //     .expect(200)
+    //     .expect(res => {
+    //         expect(res.body.todo.text).toBe(`${todos[0].text}`)
+    //     })
+    //     .end(done)
+    // })
 
+    // not found 404:
+    // it('should be valid object id but not found', done => {
+    //     request(app)
+    //     .get(`/api/todos/${new ObjectId().toHexString()}`)
+    //     .expect(404)
+    //     .end(done)
+    // })
+
+    // invalid object id: 404:
+    it('should be invalid id get 404', done => {
         request(app)
-            .post('/todos')
-            .send({ text })
-            .expect(200)
-            .expect(res => {
-                expect(res.body.text).toBe(text)
-            })
-            .end((error, res) => {
-                if (error) {
-                    return done(error);
-                }
-                Todo.find()
-                    .then(docs => {
-                        expect(docs.length).toBe(1)
-                        expect(docs[0].text).toBe(text)
-                        done()
-                    })
-                    .catch(error => done(error))
-            });
+        .get(`/api/todos/4444ssfsfsf`)
+        .expect(404)
+        .end(done)
     })
 
-    // it('should be invalid post todo', done => {
-    //     request(app)
-    //     .post('/todos')
-    //     .send({})
-    //     .expect(400)
-    //     .end((error, res)=>{
-    //         if(error){
-    //             done(error)
-    //         }
-    //         Todo.find()
-    //         .then(todos => {
-    //             expect(todos.length).toBe(0)
-    //             done()
-    //         })
-    //         .catch(error => done(error))
-    //     })
-    // })
 })
